858e777b96ac421b2c7920d3939e74b6a8b71836
Vijay Ramakrishnan <vramak@microsoft.com>
2017-05-23 11:39:18 -0700

    Updating tools

--- Publish.cmd
+++ Publish.cmd
@@ -1,33 +1,36 @@
-msbuild /t:Restore ASPNetCore\ASPNetCore.csproj 
-msbuild /t:Restore ASPNetCoreFullFramework\ASPNetCoreFullFramework.csproj 
-msbuild /t:Restore MvcApp\MvcApp.csproj 
-msbuild /t:Restore Spa\Spa.csproj 
-msbuild /t:Restore WebApiApp\WebApiApp.csproj 
-msbuild /t:Restore WebFormsApp\WebFormsApp.csproj 
-msbuild /t:Restore WebJobFullFramework\WebJobFullFramework.csproj
+dotnet restore tools\NuGet\NuGet.csproj
+if errorlevel 1 GOTO ERROR
+
+%userprofile%\.nuget\packages\nuget.commandline\4.1.0\tools\nuget.exe restore
 
 msbuild AspNetPublishSamples.sln
 if errorlevel 1 GOTO ERROR
 
-msbuild ASPNetCore\ASPNetCore.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild ASPNetCore\ASPNetCore.csproj /p:PublishUrl="..\PublishOutput\ASPNetCore" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild ASPNetCoreFullFramework\ASPNetCoreFullFramework.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild ASPNetCoreFullFramework\ASPNetCoreFullFramework.csproj /p:PublishUrl="..\PublishOutput\ASPNetCoreFullFramework" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild MvcApp\MvcApp.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild MvcApp\MvcApp.csproj /p:PublishUrl="..\PublishOutput\MvcApp" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild Spa\Spa.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild Spa\Spa.csproj /p:PublishUrl="..\PublishOutput\Spa" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild WebApiApp\WebApiApp.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild WebApiApp\WebApiApp.csproj /p:PublishUrl="..\PublishOutput\WebApiApp" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild WebFormsApp\WebFormsApp.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild WebFormsApp\WebFormsApp.csproj /p:PublishUrl="..\PublishOutput\WebFormsApp" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
-msbuild WebJobFullFramework\WebJobFullFramework.csproj /p:PublishUrl="PublishOutput" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+msbuild WebJobFullFramework\WebJobFullFramework.csproj /p:PublishUrl="..\PublishOutput\WebJobFullFramework" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+if errorlevel 1 GOTO ERROR
+
+msbuild FunctionApp\FunctionApp.csproj /p:PublishUrl="..\PublishOutput\FunctionApp" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
+if errorlevel 1 GOTO ERROR
+
+msbuild AspNetRazor3WebSite\website.publishproj /p:PublishUrl="..\PublishOutput\AspNetRazor3WebSite" /p:DeployOnBuild=true /p:Configuration=Release /p:WebPublishMethod=FileSystem /p:DeployTarget=WebPublish /p:AutoParameterizationWebConfigConnectionStrings=false /p:SolutionDir="."
 if errorlevel 1 GOTO ERROR
 
 :ERROR
---
+++ tools/NuGet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
